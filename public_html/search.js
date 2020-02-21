a755d2cdf2616d8d7043ea98561a04b1e88b493f
Fran Enguix <enguix.fco@gmail.com>
2020-02-21 12:47:33 +0100

    iframe and search

--- public_html/search.js
+++ public_html/search.js
@@ -2,19 +2,14 @@ function findQuery() {
 	var input = document.getElementById('searchInput');
 	var filter = input.value.toUpperCase(); // Not case senstive
 	var listOfDisposables = document.getElementById('listOfDisposables');
-	var items = listOfDisposables.getElementsByTabName('li');
-
-	// Only show those that match the search query
-	for (i = 0; i < items.length; i++) {
-		var link = items[i].getElementsByTagName("a");
-		var text = link.textContent || link.innerText;
-
-		// Search for filter and show results 
-		if (text.toUpperCase().indexOf(filter) > -1) {
-			items[i].style.display = "";
-		}
-		else {
-			items[i].style.display = "none";
-		}
-	}
+	var items = listOfDisposables.getElementsByTagName('li');
+        
+        for (var i = 0; i < items.length; i++) {
+            var links = items[i].getElementsByTagName('a');
+            if (links[0].innerText.toUpperCase().indexOf(filter) > -1) {
+                items[i].style.display = "";
+            } else {
+                items[i].style.display = "none";
+            }
+        }
 }
