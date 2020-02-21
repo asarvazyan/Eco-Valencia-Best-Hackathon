e3e7adba074ba9dbf7979391fab4ed750212697d
asarvazyan <areg.sarvazyan@gmail.com>
2020-02-21 16:07:01 +0100

    Adding map functionality with Leaflet

--- public_html/map.js
+++ public_html/map.js
@@ -1,8 +1,24 @@
-//window.addEventListener('load', getData);
+//window.addEventListener('load', setUpMap);
+
+
 
 
 //var types = ['Residuos Urbanos', 'Envases Ligeros', 'Organico', 'Papel \/ Carton', 'VIDRIO'];
 var types = ['Envases Ligeros', 'Organico', 'Papel \/ Carton', 'VIDRIO'];
+
+function setUpMap() {
+
+	var map = L.map('map').setView([39.74739, -105], 13);
+	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'your.mapbox.access.token'
+}).addTo(map);
+	L.geoJSON(getContainers(types[0])).addTo(map);
+}
 
 function getContainers(type) {
 	// Get map in JSON form
@@ -12,7 +28,7 @@ function getContainers(type) {
 	var mapType = mapFeatures.filter(function (entry) {
 		return entry.properties.tipo_resid === type; 
 	}); 
-	console.log(mapType);
+//	console.log(mapType);
 	return mapType;
 	/*	console.log(mapFeatures[0].properties.tipo_resid);
 	for (var i = 0; i < mapFeatures.length; i++) {
